6b083fb45b0296c33badb4f73fe691a16438b5f9
dean <deannadelacruz10@gmail.com>
2025-04-18 16:35:58 +0800

    Wazzahhhhhh

---
+++ wah.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Twinkle, twinkle, little star," << endl;
+    cout << "How I wonder what you are!" << endl;
+    cout << "Up above the world so high," << endl;
+    cout << "Like a diamond in the sky." << endl;
+    cout << "Twinkle, twinkle, little star," << endl;
+    cout << "How I wonder what you are!" << endl;
+
+    return 0;
+}
